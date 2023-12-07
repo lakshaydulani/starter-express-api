6bbc911aaca9658e6c8aaa3e2478a7ef076c1222
Lakshay Dulani <Lakshay.Dulani@in.ey.com>
2023-12-07 11:38:50 +0530

    socket

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+
+const express = require('express');
+const WebSocket = require('ws');
+
+const app = express();
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+// Create a WebSocket server
+const wss = new WebSocket.Server({ server });
+
+// Handle WebSocket connections
+wss.on('connection', (ws) => {
+  console.log('A new WebSocket connection has been established');
+
+  // Handle incoming messages from the client
+  ws.on('message', (message) => {
+    console.log('Received message:', message);
+
+    // Send a response back to the client
+    ws.send('Server received your message');
+  });
+
+  // Handle WebSocket disconnections
+  ws.on('close', () => {
+    console.log('WebSocket connection has been closed');
+  });
+});
+
+// Define your API routes
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
